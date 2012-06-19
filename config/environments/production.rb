68258817b8e6c5453016f5cd01f563314a6617b2
Dustin Secrest <dustin.secrest@gmail.com>
2012-06-19 14:57:38 -0700

    Adding browser-specific assets to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ CityUtility::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.assets.precompile += %w( application-all.css application-ie.css application-all.js application-ie.js)
 end
