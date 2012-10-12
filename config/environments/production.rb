bee024bd5ee55fb51e70b87d4cb8652dbe548dc0
Dustin Secrest <dustin.secrest@gmail.com>
2012-10-12 13:07:20 -0700

    Reorganizing stylesheets so that it's clear what's for docs only.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ CityUtility::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.assets.precompile += %w( application-all.css application-ie.css application-all.js application-ie.js)
+  config.assets.precompile += %w( application-all.css application-ie.css application-all.js application-ie.js docs.css)
 end
